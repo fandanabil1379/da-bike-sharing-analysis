5a1e22f5f80411a133c9202b0aab0d5040d47d57
Shafanda Nabil Sembodo <shafanda.nabil.s@gmail.com>
2023-06-28 23:20:17 +0700

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Design marketing strategies aimed at converting casual riders into annual members.',
+    author='shafanda nabil sembodo',
+    license='',
+)
